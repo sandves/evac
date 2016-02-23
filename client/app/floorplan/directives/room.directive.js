328bfc289550dce18ef9be8a529906ab790dcad4
sandves <stian@sandve.org>
2016-02-23 16:50:45 +0100

    Made a room directive

---
+++ client/app/floorplan/directives/room.directive.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.floorplan')
+        .directive('evacRoom', roomDirective);
+
+    function roomDirective() {
+
+        var directive = {
+            bindToController: true,
+            templateUrl: 'app/floorplan/directives/room.html',
+            controller: RoomController,
+            controllerAs: 'vm',
+            link: link,
+            restrict: 'EA',
+            scope: {
+                room: '='
+            }
+        };
+        return directive;
+        
+        function link(scope, element, attrs) {
+            
+        }
+    }
+    /* @ngInject */
+    function RoomController () {
+        var vm = this;
+        var room = vm.room;
+        
+        vm.roomStyle = {
+            'width': room.width + 'px',
+            'height': room.height + 'px',
+            'top': room.top + 'px',
+            'left': room.left + 'px',
+            'line-height': room.height + 'px'
+        };
+    }
+})();
--- client/app/floorplan/floorplan.controller.js
+++ client/app/floorplan/floorplan.controller.js
@@ -8,6 +8,21 @@
     function FloorplanController() {
         var vm = this;
 
-        vm.rooms = ['1012', '1013'];
+        vm.rooms = [
+            {
+                name: 'Foo',
+                width: 100,
+                height: 50,
+                top: 20,
+                left: 20
+            },
+            {
+                name: 'Bar',
+                width: 50,
+                height: 100,
+                top: 100,
+                left: 20
+            }
+        ];
     }
 })();
